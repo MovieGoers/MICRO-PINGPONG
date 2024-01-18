de1d012bc07b83250a612a47bb94b21c84d684dc
MovieGoers <alscjf4104@gmail.com>
2024-01-18 19:45:35 +0900

    feat : 화면에 맞춰서 Player 드래그 되도록 추가.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     private static InputManager instance;
+    private GameObject player;
+
+    public float mouseZ;
     public static InputManager Instance
     {
         get { return instance; }
+    }
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(instance);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        player = GameObject.Find("Player");
     }
 
     // Start is called before the first frame update
@@ -19,10 +35,13 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DragPlayer();
     }
 
-    private void OnMouseDrag()
+    private void DragPlayer()
     {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mouseZ);
+        Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        player.transform.position = new Vector3(objectPosition.x, objectPosition.y, player.transform.position.z);
     }
 }
