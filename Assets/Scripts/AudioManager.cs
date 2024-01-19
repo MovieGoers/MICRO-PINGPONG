a0feb82f5b1974ff1e79767037d7c434c8b1f8e3
MovieGoers <alscjf4104@gmail.com>
2024-01-19 23:31:31 +0900

    init : Audio Manager, Sound 구현

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    public Sound[] sounds;
+    public static AudioManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(instance);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        foreach(Sound s in sounds) // 각 sound 객체의 AudioSource 컴포넌트 생성 및 여러 세팅.
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+
+        }
+    }
+    
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name); // sounds 배열 내 name과 같은 이름을 가진 sound 찾기.
+
+        if(s == null) // sound를 찾지 못한 경우,
+        {
+            Debug.LogWarning("Sound : " + name + " Not Found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+}
