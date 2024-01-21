13960d2c7290cc52f5a9fc535b85cfc8ffcfd234
MovieGoers <alscjf4104@gmail.com>
2024-01-22 01:50:13 +0900

    added : 리더보드 매니저 추가.

---
+++ Assets/Scripts/LeaderboardsManager.cs
@@ -0,0 +1,107 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Leaderboards;
+using UnityEngine;
+
+public class LeaderboardsManager : MonoBehaviour
+{
+    // Create a leaderboard with this ID in the Unity Cloud Dashboard
+    const string LeaderboardId = "micro-pong-leaderboard";
+
+    string VersionId { get; set; }
+    int Offset { get; set; }
+    int Limit { get; set; }
+    int RangeLimit { get; set; }
+    List<string> FriendIds { get; set; }
+
+    private static LeaderboardsManager instance;
+
+    public static LeaderboardsManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    async void Awake()
+    {
+        if (instance)
+        {
+            Destroy(instance);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        await UnityServices.InitializeAsync();
+
+        await SignInAnonymously();
+    }
+
+    async Task SignInAnonymously()
+    {
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in as: " + AuthenticationService.Instance.PlayerId);
+        };
+        AuthenticationService.Instance.SignInFailed += s =>
+        {
+            // Take some action here...
+            Debug.Log(s);
+        };
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    public async void AddScore(int score)
+    {
+        var scoreResponse = await LeaderboardsService.Instance.AddPlayerScoreAsync(LeaderboardId, score);
+        Debug.Log(JsonConvert.SerializeObject(scoreResponse));
+    }
+
+    public async void GetScores()
+    {
+        var scoresResponse =
+            await LeaderboardsService.Instance.GetScoresAsync(LeaderboardId);
+        Debug.Log(JsonConvert.SerializeObject(scoresResponse));
+    }
+
+    public async void GetPaginatedScores()
+    {
+        Offset = 10;
+        Limit = 10;
+        var scoresResponse =
+            await LeaderboardsService.Instance.GetScoresAsync(LeaderboardId, new GetScoresOptions { Offset = Offset, Limit = Limit });
+        Debug.Log(JsonConvert.SerializeObject(scoresResponse));
+    }
+
+    public async void GetPlayerScore()
+    {
+        var scoreResponse =
+            await LeaderboardsService.Instance.GetPlayerScoreAsync(LeaderboardId);
+        Debug.Log(JsonConvert.SerializeObject(scoreResponse));
+    }
+
+    public async void GetVersionScores()
+    {
+        var versionScoresResponse =
+            await LeaderboardsService.Instance.GetVersionScoresAsync(LeaderboardId, VersionId);
+        Debug.Log(JsonConvert.SerializeObject(versionScoresResponse));
+    }
+
+    public async void GetPlayerRange()
+    {
+        // Returns a total of 11 entries (the given player plus 5 on either side)
+        var rangeLimit = 5;
+        var scoresResponse = await LeaderboardsService.Instance.GetPlayerRangeAsync(
+            LeaderboardId,
+            new GetPlayerRangeOptions { RangeLimit = rangeLimit }
+        );
+        Debug.Log(JsonConvert.SerializeObject(scoresResponse));
+    }
+}
